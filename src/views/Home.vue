92f5b5af2ebfe8ac53a3c9478e7ea24ebd51319f
thsuekfk2 <76821963+thsuekfk2@users.noreply.github.com>
2021-02-01 00:07:31 +0900

    asdf

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  <div>
+<h1>This is Home page</h1>
+<hyebin title="home title"/>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import hyebin from '@/components/Hyebin.vue';
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  components : {
+    hyebin
+  },
+  data(){
+    return{
+      name: "hyebin"
+    }
   }
 }
 </script>
+
+<style scoped>
+h1{
+  color: red;
+}
+</style>
